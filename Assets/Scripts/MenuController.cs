75bace26f3ae68711fd77c3caa37710300a0ccfc
Marcelo Soto <92414181+Siddhartha-86@users.noreply.github.com>
2022-12-26 21:48:32 -0300

    ControlesUI Scene udpate
    Actualizado con nuevo GameObject MenuController para sliderVolume mejorado.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Opciones de volumen")]
+    [SerializeField] private Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+
+
+
+
     [Header("Niveles a cargar")]
     public string _newGameLevel;
     private string levelToLoad;
@@ -35,4 +46,23 @@ public class MenuController : MonoBehaviour
         Application.Quit();
     }
 
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        //Mostrar una pantalla
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
+    }
 }
